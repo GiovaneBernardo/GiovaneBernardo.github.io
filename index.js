8603afc86847c3c0a51c78ae9174008d67d8e1cc
YlaxusB <souzagiovane2019@gmail.com>
2024-12-13 04:15:27 -0300

    updated plaza engine description and remove unused assets

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const projects = document.querySelectorAll(".project-text-container");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const projects = document.querySelectorAll(".project-text-container");
 
-  projects.forEach((project) => {
-    project.addEventListener("click", () => {
-      //project.classList.toggle("expanded");
-      project.parentElement
-        .querySelector(".project-details")
-        .classList.toggle("expanded");
-    });
-  });
+//   projects.forEach((project) => {
+//     project.addEventListener("click", () => {
+//       //project.classList.toggle("expanded");
+//       project.parentElement
+//         .querySelector(".project-details")
+//         .classList.toggle("expanded");
+//     });
+//   });
 
-  const closeButtons = document.querySelectorAll(".project-modal-close-button");
+//   const closeButtons = document.querySelectorAll(".project-modal-close-button");
 
-  closeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      button.parentElement.parentElement.parentElement.classList.toggle("expanded");
-    });
-  });
-});
+//   closeButtons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//       button.parentElement.parentElement.parentElement.classList.toggle("expanded");
+//     });
+//   });
+// });
 
 
